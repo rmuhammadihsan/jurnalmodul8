a8d210c68906a51ea433ab653c04369cadbe5d0d
rmuhammadihsan <rmuhammadihsan01@gmail.com>
2017-04-06 09:16:53 +0700

    Update Jurnal.java

--- Jurnal.java
+++ Jurnal.java
@@ -20,17 +20,18 @@ public class Jurnal {
         // TODO code application logic here
         Scanner sc=new Scanner(System.in);
         int n1=1,n2=1,n3,i;    
+        int hasil=n1+n2;
         System.out.println("Masukkan Inputan : ");
         int count=sc.nextInt();
-        System.out.print(n1+" "+n2);//printing 0 and 1    
-    
+       
        for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
         {    
             n3=n1+n2;    
-            System.out.print(" "+n3);    
+            hasil+=n3;    
             n1=n2;    
             n2=n3;    
-        }    
+        }   
+         System.out.print(hasil);
     }
     
 }
